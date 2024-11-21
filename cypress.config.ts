5e1c01b3cea36e2fb5b23e3732e06f4b4cbd227d
KohKeira <kohkeira996@gmail.com>
2024-11-21 11:45:10 +0800

    (feat) Set up testing environment for cypress
    Run both `php artisan serve` and `npm run dev` using `npm start`. Create a database migration and seeder task for cypress. Run `cypress run` using `npm run cy:run`

---
+++ cypress.config.ts
@@ -0,0 +1,29 @@
+import { exec, spawn } from 'child_process';
+import { defineConfig } from 'cypress';
+let server;
+
+export default defineConfig({
+    e2e: {
+        baseUrl: 'http://localhost:8000',
+        setupNodeEvents(on, config) {
+            on('task', {
+                artisanMigrateFreshSeed() {
+                    return new Promise((resolve, reject) => {
+                        exec(
+                            'php artisan migrate:fresh --seed',
+                            (error, stdout, stderr) => {
+                                if (error) {
+                                    console.error(`Error: ${stderr}`);
+                                    return reject(stderr);
+                                }
+                                console.log(`Output: ${stdout}`);
+                                resolve(stdout);
+                            },
+                        );
+                    });
+                },
+            });
+
+        },
+    },
+});
