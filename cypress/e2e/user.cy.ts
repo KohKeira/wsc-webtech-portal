a295d5ff0cba5e382e989169fcb49b1ebe35ca10
KohKeira <kohkeira996@gmail.com>
2024-11-21 15:19:10 +0800

    (test) Add more test cases for add user function
    Test cases includes validation for the following
    - phone number format
    - email of student
    - required fields
    - duplicated user

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -1,4 +1,33 @@
 describe('Add User Functionality', () => {
+    const phoneNumberTestCases = [
+        {
+            phoneNumber: '12345678',
+            description:
+                'should not add user if phone number does not start with 6, 8 or 9',
+            errorMessage: 'The phone number field format is invalid.',
+        },
+        {
+            phoneNumber: '2345678',
+            description:
+                'should not add user if phone number does not have 8 digits',
+            errorMessage: 'The phone number field format is invalid.',
+        },
+        {
+            phoneNumber: '62345678',
+            description: 'should add user if phone number starts with 6',
+            errorMessage: null,
+        },
+        {
+            phoneNumber: '82345678',
+            description: 'should add user if phone number starts with 8',
+            errorMessage: null,
+        },
+        {
+            phoneNumber: '92345678',
+            description: 'should add user if phone number starts with 9',
+            errorMessage: null,
+        },
+    ];
     beforeEach(() => {
         cy.task('artisanMigrateFreshSeed').then((output) => {
             cy.log(output as string);
@@ -21,27 +50,130 @@ describe('Add User Functionality', () => {
         cy.contains('Add User').should('exist');
 
         // fill up the form
-        cy.get('input[name="role"]').check('student');
-        cy.get('input[name="role"]').should('have.value', 'student');
-
-        cy.get('input[name="name"]').type('lily_kim');
-        cy.get('input[name="name"]').should('have.value', 'lily_kim');
-
-        cy.get('input[name="email"]').type('2301234B');
-        cy.get('input[name="email"]').should('have.value', '2301234B');
-
-        cy.get('input[name="gender"]').check('female');
-        cy.get('input[name="gender"]').should('have.value', 'female');
-
-        cy.get('input[name="phone_number"]').type('90123456');
-        cy.get('input[name="phone_number"]').should('have.value', '90123456');
+        cy.fillUserForm(
+            'student',
+            'lily_kim',
+            '2301234B',
+            'female',
+            '90123456',
+        );
 
         // submit form
-        cy.get('button').contains('Add').click();
+        cy.get('[data-cy="add-button"]').click();
 
         // check if redirected to users page with success message
         cy.url().should('match', /users/);
         cy.contains('User created successfully').should('exist');
+    });
+    it('should not add new user with empty fields', () => {
+        //login with lecturer account
+        cy.login('ana_yap@tp.edu.sg', 'test');
+
+        cy.visit('users/create');
+        cy.get('[data-cy="add-button"]').click();
+
+        // ensure user is still on the add user form
+        cy.url().should('match', /users\/create/);
+
+        // validation errors for required inputs
+        cy.get('input[name="name"]').then(($input) => {
+            expect(($input[0] as HTMLInputElement).validationMessage).to.eq(
+                'Please fill out this field.',
+            );
+        });
+        cy.get('input[name="name"]').then(($input) => {
+            expect(($input[0] as HTMLInputElement).validationMessage).to.eq(
+                'Please fill out this field.',
+            );
+        });
+        cy.get('input[name="email"]').then(($input) => {
+            expect(($input[0] as HTMLInputElement).validationMessage).to.eq(
+                'Please fill out this field.',
+            );
+        });
+        cy.get('input[name="phone_number"]').then(($input) => {
+            expect(($input[0] as HTMLInputElement).validationMessage).to.eq(
+                'Please fill out this field.',
+            );
+        });
+    });
+    it('should not add user with invalid student email', () => {
+        //login with lecturer account
+        cy.login('ana_yap@tp.edu.sg', 'test');
+
+        cy.visit('users/create');
+
+        cy.fillUserForm('student', 'lily_kim', '2301234', 'female', '90123456');
+
+        cy.get('[data-cy="add-button"]').click();
+
+        cy.url().should('match', /users\/create/);
+
+        cy.get('[data-cy="email-error"]')
+            .should('exist')
+            .and('contain', 'Student email format is invalid');
+    });
+    phoneNumberTestCases.forEach((test) => {
+        it(test.description, () => {
+            //login with lecturer account
+            cy.login('ana_yap@tp.edu.sg', 'test');
+
+            cy.visit('users/create');
+
+            cy.fillUserForm(
+                'student',
+                'lily_kim',
+                '2301234B',
+                'female',
+                test.phoneNumber,
+            );
+
+            cy.get('[data-cy="add-button"]').click();
+
+            if (test.errorMessage) {
+                cy.url().should('match', /users\/create/);
+
+                cy.get('[data-cy="phone-error"]')
+                    .should('exist')
+                    .and(
+                        'contain',
+                        'The phone number field format is invalid.',
+                    );
+            } else {
+                cy.url().should('match', /users/);
+
+                cy.get('[data-cy="phone-error"]').should('not.exist');
+            }
+        });
+    });
+    it('should not add user with duplicated email, name and phone_number', () => {
+        //login with lecturer account
+        cy.login('ana_yap@tp.edu.sg', 'test');
+
+        cy.visit('users/create');
+
+        cy.fillUserForm(
+            'student',
+            'James Lee',
+            '2301234A',
+            'female',
+            '91234567',
+        );
+
+        cy.get('[data-cy="add-button"]').click();
+
+        cy.url().should('match', /users\/create/);
+
+        // validation error for duplicated user
+        cy.get('[data-cy="email-error"]')
+            .should('exist')
+            .and('contain', 'The email has already been taken.');
+        cy.get('[data-cy="name-error"]')
+            .should('exist')
+            .and('contain', 'The name has already been taken.');
+        cy.get('[data-cy="phone-error"]')
+            .should('exist')
+            .and('contain', 'The phone number has already been taken.');
     });
     it('should redirect to login page when accessing the Add User Page', () => {
         cy.visit('/users/create');
