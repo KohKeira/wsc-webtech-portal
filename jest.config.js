1d702183b1eb364a6618b2f3f038d235bc6ee77d
KohKeira <kohkeira996@gmail.com>
2024-12-02 13:51:14 +0800

    test: Update code coverage threshold
    Increase to 90% from 80%

--- jest.config.js
+++ jest.config.js
@@ -17,10 +17,10 @@ export default {
     collectCoverageFrom: ['resources/js/Pages/Users/*.{ts,tsx}'],
     coverageThreshold: {
         global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: 80,
+            branches: 90,
+            functions: 90,
+            lines: 90,
+            statements: 90,
         },
     },
 };
